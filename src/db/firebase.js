f0e2465b0c32280f9a940a27be817977308fa054
bryanvanmoer <bryan.vanmoer51@hotmail.com>
2022-11-26 12:49:47 +0100

    delete a todo

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -1,116 +1,130 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc, getDoc, getDocs, orderBy, query, addDoc, Timestamp, updateDoc, getCountFromServer, deleteDoc } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import {
+	getFirestore,
+	collection,
+	doc,
+	getDoc,
+	getDocs,
+	orderBy,
+	query,
+	addDoc,
+	Timestamp,
+	updateDoc,
+	getCountFromServer,
+	deleteDoc
+} from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCCLwJYYdI4hUgRbkUlPXVpy7JenrIP92Q",
-  authDomain: "svelte-project-manager.firebaseapp.com",
-  projectId: "svelte-project-manager",
-  storageBucket: "svelte-project-manager.appspot.com",
-  messagingSenderId: "96941287208",
-  appId: "1:96941287208:web:3535db4c46df2eb722121d"
+	apiKey: 'AIzaSyCCLwJYYdI4hUgRbkUlPXVpy7JenrIP92Q',
+	authDomain: 'svelte-project-manager.firebaseapp.com',
+	projectId: 'svelte-project-manager',
+	storageBucket: 'svelte-project-manager.appspot.com',
+	messagingSenderId: '96941287208',
+	appId: '1:96941287208:web:3535db4c46df2eb722121d'
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
-const projectsCollection = collection(db, "projects");
+const projectsCollection = collection(db, 'projects');
 
 async function getProjects() {
-  let firebaseProjects = [];
-  const querySnapshot = await getDocs(projectsCollection);
-  querySnapshot.forEach((doc) => {
-    let project = {...doc.data(), id: doc.id}
-    firebaseProjects = [project, ...firebaseProjects]
-  });
-  return firebaseProjects;
+	let firebaseProjects = [];
+	const querySnapshot = await getDocs(projectsCollection);
+	querySnapshot.forEach((doc) => {
+		let project = { ...doc.data(), id: doc.id };
+		firebaseProjects = [project, ...firebaseProjects];
+	});
+	return firebaseProjects;
 }
 
 async function getProject(id) {
-  const docSnap = await getDoc(doc(db, "projects", id));
-  if (docSnap.exists()) {
-    console.log("Document data:", docSnap.data());
-    return docSnap.data()
-  } else {
-    console.error("No such document!");
-  }
+	const docSnap = await getDoc(doc(db, 'projects', id));
+	if (docSnap.exists()) {
+		console.log('Document data:', docSnap.data());
+		return docSnap.data();
+	} else {
+		console.error('No such document!');
+	}
 }
 
-async function getTodos(id){
-  let fbTodos = [];
-  const queryTodos = query(collection(db, "projects", id, "todos"), orderBy('created', 'desc'));
-  const querySnapshot = await getDocs(queryTodos);
-  querySnapshot.forEach((doc) => {
-    let todo = {...doc.data(), id: doc.id};
-    fbTodos = [todo, ...fbTodos]
-  });
-  return fbTodos;
+async function getTodos(id) {
+	let fbTodos = [];
+	const queryTodos = query(collection(db, 'projects', id, 'todos'), orderBy('created', 'desc'));
+	const querySnapshot = await getDocs(queryTodos);
+	querySnapshot.forEach((doc) => {
+		let todo = { ...doc.data(), id: doc.id };
+		fbTodos = [todo, ...fbTodos];
+	});
+	return fbTodos;
 }
 
-async function addTodo(projId, task){
-  const todosCollection = collection(db, "projects", projId, "todos");
-  const docRef = await addDoc(todosCollection,{
-    task: task,
-    completed: false,
-    created: Timestamp.fromDate(new Date())
-  });
-  console.log("Document written with ID: ", docRef.id);
+async function addTodo(projId, task) {
+	const todosCollection = collection(db, 'projects', projId, 'todos');
+	const docRef = await addDoc(todosCollection, {
+		task: task,
+		completed: false,
+		created: Timestamp.fromDate(new Date())
+	});
+	console.log('Document written with ID: ', docRef.id);
 }
 
-async function toggleCompleted(projId, todoId, completed){
-  const completedRef = doc(db, "projects", projId, "todos", todoId);
-  console.log("toggled a todo")
-  await updateDoc(completedRef, {
-    completed: completed
-  })
+async function toggleCompleted(projId, todoId, completed) {
+	const completedRef = doc(db, 'projects', projId, 'todos', todoId);
+	console.log('toggled a todo');
+	await updateDoc(completedRef, {
+		completed: completed
+	});
 }
 
-async function countTodos(projId){
-  const todosCollection = collection(db, "projects", projId, "todos");
-  const snapshot = await getCountFromServer(todosCollection);
-  return snapshot.data().count;
+async function countTodos(projId) {
+	const todosCollection = collection(db, 'projects', projId, 'todos');
+	const snapshot = await getCountFromServer(todosCollection);
+	return snapshot.data().count;
 }
 
-async function editProject(projId, title, description, deadline){
-  const projectRef = doc(db, "projects", projId);
-  console.log("edited a project")
-  await updateDoc(projectRef, {title: title, description: description, deadline: deadline});
+async function editProject(projId, title, description, deadline) {
+	const projectRef = doc(db, 'projects', projId);
+	console.log('edited a project');
+	await updateDoc(projectRef, { title: title, description: description, deadline: deadline });
 }
 
-async function createProject(title, description, deadline){
-  const docRef = await addDoc(projectsCollection, {
-    title: title, 
-    description: description,
-    deadline: deadline
-  })
-  console.log("Document created with ID: ", docRef.id);
+async function createProject(title, description, deadline) {
+	const docRef = await addDoc(projectsCollection, {
+		title: title,
+		description: description,
+		deadline: deadline
+	});
+	console.log('Document created with ID: ', docRef.id);
 }
 
-async function deleteProject(projId){
-  await deleteDoc(doc(db, "projects", projId));
-  console.log("project " + projId + " deleted");
+async function deleteProject(projId) {
+	await deleteDoc(doc(db, 'projects', projId));
+	console.log('project ' + projId + ' deleted');
+}
+
+async function deleteTodo(projectId, todoId) {
+	await deleteDoc(doc(db, 'projects', projectId, 'todos', todoId));
 }
 //make a svelte form with 2 text input and a date input?
 
-export{
-  db,
-  projectsCollection,
-  getProjects,
-  getProject,
-  getTodos,
-  addTodo,
-  toggleCompleted,
-  countTodos,
-  editProject,
-  createProject,
-  deleteProject
-}
-
-
-
-
+export {
+	db,
+	projectsCollection,
+	getProjects,
+	getProject,
+	getTodos,
+	addTodo,
+	toggleCompleted,
+	countTodos,
+	editProject,
+	createProject,
+	deleteProject,
+	deleteTodo
+};
