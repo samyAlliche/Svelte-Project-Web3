fed0410fd980cef02d3b2ad02c7850cc83657bd3
samyAlliche <samy.alliche.22@gmail.com>
2022-11-26 13:59:05 +0100

    fixed edit project + refactor

---
+++ src/components/ProjectDeadline.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let deadline;
+    const timeLeft = Math.ceil(Math.abs(new Date(deadline.toDate()) - new Date())/(1000*3600*24));
+</script>
+
+<div class="pf-deadline">{deadline.toDate().toDateString()} ({timeLeft} days left)</div>
+
+<style>
+    .pf-deadline{
+        display:inline-block;
+        color: gray;
+        padding: 1rem;
+        font-size: 0.95rem;
+        font-family: 'Roboto Mono', monospace;
+        font-weight: 300;
+    }
+</style>
